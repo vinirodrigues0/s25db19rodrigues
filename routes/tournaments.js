4b3b4e974e7f3b9a5b38a8cee7f8bbe1a823d288
Vinicius Rodrigues <S543619@NWMISSOURI.EDU>
2025-04-21 23:38:43 -0500

    Assignment 13: Bonus 2 done

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -17,9 +17,9 @@ router.get('/tournaments/:id', tournament_controller.tournament_detail);
 router.put('/tournaments/:id', tournament_controller.tournament_update_put);
 router.delete('/tournaments/:id', tournament_controller.tournament_delete);
 router.get('/detail', tournament_controller.tournament_view_one_Page);
-router.get('/create', tournament_controller.tournament_create_Page);
+router.get('/create', secured, tournament_controller.tournament_create_Page);
 router.get('/update', secured, tournament_controller.tournament_update_Page);
-router.get('/delete', tournament_controller.tournament_delete_Page);
+router.get('/delete', secured, tournament_controller.tournament_delete_Page);
 router.post('/login', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
 });
