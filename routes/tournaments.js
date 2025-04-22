6f1d04f34068acacb2afb0aefd04b8488a822064
Vinicius Rodrigues <S543619@NWMISSOURI.EDU>
2025-04-21 23:09:58 -0500

    Assignment 13: Part 4 done

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -2,6 +2,14 @@ var express = require('express');
 const tournament_controller = require('../controllers/tournament');
 const tournament = require('../models/tournament');
 var router = express.Router();
+const passport = require('passport');
+
+const secured = (req, res, next) => {
+    if(req.user) {
+        return next();
+    }
+    res.redirect("/login");
+};
 
 router.get('/', tournament_controller.tournament_view_all_Page);
 router.post('/', tournament_controller.tournament_create_post);
@@ -10,7 +18,10 @@ router.put('/tournaments/:id', tournament_controller.tournament_update_put);
 router.delete('/tournaments/:id', tournament_controller.tournament_delete);
 router.get('/detail', tournament_controller.tournament_view_one_Page);
 router.get('/create', tournament_controller.tournament_create_Page);
-router.get('/update', tournament_controller.tournament_update_Page);
+router.get('/update', secured, tournament_controller.tournament_update_Page);
 router.get('/delete', tournament_controller.tournament_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
 
 module.exports = router;
